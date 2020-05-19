7f2a95a797e0489422c36759e5b66f1f167e15ae
Nemezisp <47280559+Nemezisp@users.noreply.github.com>
2020-05-19 21:33:18 +0200

    fix

--- src/components/square/square.component.jsx
+++ src/components/square/square.component.jsx
@@ -7,7 +7,7 @@ export const Square = ({color, piece, canBeMovedTo, index, click, size, reversed
     let reversedClass = reversed ? 'reversed' : '';
 
     let toRender = piece.number ? 
-                   <img className = {reversedClass} alt = {piece.icon} src = {require('./../pieces/piece-icons/' + piece.icon)}/> : <div style = {{height: size, width: size}}/>;
+                   <img className = {reversedClass} alt = {piece.icon} src = {require('./../pieces/piece-icons/' + piece.icon)}/> : <div style = {{height: size, width: size}}>&nbsp;</div>;
 
     let onClick = click ? () => click(piece, index) : null
 
