7a0d1d45b799bbc99181c7281f0eb2e8197cd7c9
Nemezisp <47280559+Nemezisp@users.noreply.github.com>
2020-05-28 20:16:36 +0200

    fix heroku server activation

--- src/components/startPage/startPage.component.jsx
+++ src/components/startPage/startPage.component.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import './startPage.styles.css';
 import {connect} from 'react-redux';
+import {socket} from './../socket';
 import ChooseButton from './../chooseButton/chooseButton.component';
 import {setLocalGame, setOnlineGame} from '../../redux/actions';
 
 const StartPage = ({setLocalGame, setOnlineGame}) => {
+
+    const onOnlineGameChosen = () => {
+        socket.emit('activate-server')
+        setLocalGame()
+    }
+
     return (
         <div className = 'start-page'>
-            <ChooseButton onClick = {() => setLocalGame()} textOnButton = 'Local play'/>
-            <ChooseButton onClick = {() => setOnlineGame()} textOnButton = 'Online play'/>
+            <span className = 'local-play-button'><ChooseButton onClick = {() => onOnlineGameChosen()} textOnButton = 'Local play'/></span>
+            <span className = 'online-play-button'><ChooseButton onClick = {() => setOnlineGame()} textOnButton = 'Online play'/></span>
         </div>
     )
 }
